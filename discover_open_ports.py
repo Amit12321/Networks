738ae931d8e6df19d25abf25869e504ca25d5286
Amit12321 <70763233+Amit12321@users.noreply.github.com>
2020-10-19 02:45:35 +0300

    Update discover_open_ports.py

--- discover_open_ports.py
+++ discover_open_ports.py
@@ -3,7 +3,7 @@ import sys
 
 DST = sys.argv[1]
 
-for i in range(1500, 1800):
+for i in range(20, 1030):
 	syn = TCP(dport=i, seq=123, flags='S')
 	syn_packet = IP(dst=DST) / syn
 	res = sr1(syn_packet, timeout=2, verbose=0)
